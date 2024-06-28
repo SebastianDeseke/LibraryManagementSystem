e73596d05440db8b022b2a3119b4c622cea76270
Sebastian Deseke <deseke97@gmail.com>
2024-06-28 16:02:40 +0200

    Database Controller methods added

--- Controllers/DbController.cs
+++ Controllers/DbController.cs
@@ -1,7 +1,9 @@
+using LibraryManagementSystem.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using System.Collections;
+using System.Data;
 //using Microsoft.EntityFrameworkCore;
 
 namespace LibraryManagementSystem.Controllers;
@@ -10,9 +12,11 @@ namespace LibraryManagementSystem.Controllers;
 public class DbController : Controller{
     MySqlConnection connection { get; set; }
     private readonly IConfiguration _config;
+    private readonly ILogger<DbController> _logger;
 
-    public DbController(IConfiguration config){
+    public DbController (IConfiguration config, ILogger<DbController> logger){
         _config = config;
+        _logger = logger;
     }
 
     public void Connect () {
@@ -22,6 +26,93 @@ public class DbController : Controller{
 
     public void Disconnect () {
         connection.Close();
+    }
+
+    public List<Member> GetAllMembers () {
+        Connect();
+        List<Member> members = new();
+        var cmd = connection.CreateCommand();
+        cmd.CommandText = "SELECT * FROM members";
+        var reader = cmd.ExecuteReader();
+        while (reader.Read()) {
+            members.Add(reader.GetFieldValue<Member>(0));
+            Console.WriteLine(reader.GetString(0));
+        }
+        Disconnect();
+        return members;
+    }
+
+    public List<BookLoan> GetAllLoans () {
+        Connect();
+        List<BookLoan> loans = new();
+        var cmd = connection.CreateCommand();
+        cmd.CommandText = "SELECT * FROM loans";
+        var reader = cmd.ExecuteReader();
+        while (reader.Read()) {
+            loans.Add(reader.GetFieldValue<BookLoan>(0));
+            Console.WriteLine(reader.GetString(0));
+        }
+        Disconnect();
+        return loans;
+    }
+
+    public List<Book> GetAllBooks () {
+        Connect();
+        List<Book> books = new();
+        var cmd = connection.CreateCommand();
+        cmd.CommandText = "SELECT * FROM books";
+        var reader = cmd.ExecuteReader();
+        while (reader.Read()) {
+            books.Add(reader.GetFieldValue<Book>(0));
+            Console.WriteLine(reader.GetString(0));
+        }
+        Disconnect();
+        return books;
+    }
+
+    public Member GetMember (int id) {
+        Connect();
+        Member member;
+        var cmd = connection.CreateCommand();
+        cmd.CommandText = "SELECT * FROM members WHERE id = @id";
+        cmd.Parameters.AddWithValue("@id", id);
+        var reader = cmd.ExecuteReader();
+        while (reader.Read()) {
+            member = reader.GetFieldValue<Member>(0);
+            Console.WriteLine(reader.GetString(0));
+        }
+        Disconnect();
+        return member;
+    }
+
+    public BookLoan GetLoan (int id) {
+        Connect();
+        BookLoan loan;
+        var cmd = connection.CreateCommand();
+        cmd.CommandText = "SELECT * FROM loans WHERE id = @id";
+        cmd.Parameters.AddWithValue("@id", id);
+        var reader = cmd.ExecuteReader();
+        while (reader.Read()) {
+            loan = reader.GetFieldValue<BookLoan>(0);
+            Console.WriteLine(reader.GetString(0));
+        }
+        Disconnect();
+        return loan;
+    }
+
+    public Book GetBook (int id) {
+        Connect();
+        Book book;
+        var cmd = connection.CreateCommand();
+        cmd.CommandText = "SELECT * FROM books WHERE id = @id";
+        cmd.Parameters.AddWithValue("@id", id);
+        var reader = cmd.ExecuteReader();
+        while (reader.Read()) {
+            book = reader.GetFieldValue<Book>(0);
+            Console.WriteLine(reader.GetString(0));
+        }
+        Disconnect();
+        return book;
     }
 }
 
--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -1,3 +1,5 @@
+using K4os.Compression.LZ4.Internal;
+using LibraryManagementSystem.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,6 +8,34 @@ namespace LibraryManagementSystem.Controllers;
 [Authorize]
 [ApiController]
 [Route("api/[controller]")]
-public class MemberController : ControllerBase {
+public class MemberController : ControllerBase
+{
+    private readonly DbController _db;
+    private readonly IConfiguration _config;
+    private readonly ILogger<MemberController> _logger;
+    private Member member;
 
+    public MemberController (IConfiguration config, ILogger<MemberController> logger, DbController db)
+    {
+        _config = config;
+        _db = db;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Member>>> GetMembers()
+    {
+        return _db.GetAllMembers();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Member>> GetMember(int id)
+    {
+        member = _db.GetMember(id);
+        if (member == null)
+        {
+            return NotFound();
+        }
+        return member;
+    }
 }
